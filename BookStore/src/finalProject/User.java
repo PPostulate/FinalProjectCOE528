5ba076c0806a7b5279d70c63600cfef5354cb3f3
myasui <myasui@cmp55.ee.ryerson.ca>
2025-03-03 18:12:31 -0500

    Initial Commit

---
+++ BookStore/src/finalProject/User.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalProject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author myasui
+ */
+public abstract class User {
+    public abstract void startScene(Stage stage);
+    public abstract Scene logout(Stage stage);
+}
