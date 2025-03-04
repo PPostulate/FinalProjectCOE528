2a2de669501b83bb855999f97383b5826a9c864d
szyang <szyang@cmp16.ee.ryerson.ca>
2025-03-03 20:36:39 -0500

    Gave up and went to play rocket league

---
+++ BookStore/src/finalProject/Database.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalProject;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter; 
+
+class bookData{
+    
+
+}
+
+
+/**
+ *
+ * @author szyang
+ */
+public class Database {
+    private static FileWriter customerW;
+    private static FileReader customerR; 
+    private static BufferedReader reader;
+    
+}
