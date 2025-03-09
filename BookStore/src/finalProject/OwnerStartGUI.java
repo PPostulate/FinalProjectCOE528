6dd29d587635b8e73a18676a4563530f231cf28c
szyang <szyang@cmp16.ee.ryerson.ca>
2025-03-09 12:45:14 -0400

    Created the owner screen template

--- BookStore/src/finalProject/OwnerStartGUI.java
+++ BookStore/src/finalProject/OwnerStartGUI.java
@@ -4,7 +4,18 @@
  */
 package finalProject;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -12,9 +23,36 @@ import javafx.stage.Stage;
  */
 public class OwnerStartGUI extends GUIMode{
     
+   
+    
     @Override
     public void start (Stage stage, Driver d){
-        System.out.println("Working");
+        GridPane ownerGridPane = new GridPane();
+        ownerGridPane.setPadding(new Insets(20));
+        ownerGridPane.setAlignment(Pos.CENTER);
+        ownerGridPane.setHgap(10);
+        ownerGridPane.setVgap(10);
+        
+        VBox buttonBox = new VBox(10); 
+        buttonBox.setAlignment(Pos.CENTER);
+        
+        Button bookButton = new Button("Books"); 
+        Button customerButton = new Button("Customers"); 
+        Button logoutButton = new Button("Logout"); 
+        
+        double buttonWidth = 150; 
+        bookButton.setPrefWidth(buttonWidth);
+        customerButton.setPrefWidth(buttonWidth);
+        logoutButton.setPrefWidth(buttonWidth);
+        
+        
+        buttonBox.getChildren().addAll(bookButton,customerButton,logoutButton); 
+        ownerGridPane.add(buttonBox, 0, 0);
+        
+        Scene scene = new Scene(ownerGridPane, 400, 200);
+        stage.setTitle("Bookstore App [Admin]");
+        stage.setScene(scene);
+        stage.show();
         
     }
 }
